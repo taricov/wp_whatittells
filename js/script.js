d82d0532e95b8666e24f7fdba65c6d722f602a17
TaricOv <tareq.ouf@gmail.com>
2022-09-03 16:28:18 +0200

    Added the scrollToTop btn and make the tabs sticky to the top when scrolled down

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 $(document).ready(() => {
 
+
+
+
+    var scrollToTopIcon = $(".scrollToTopIcon");
+    console.log(scrollToTopIcon)
+    $(window).scroll(function () {
+        var topPos = $(this).scrollTop();
+        if (topPos > 100) {
+            scrollToTopIcon.css("opacity", "1");
+        } else {
+            scrollToTopIcon.css("opacity", "0");
+        }
+    });
+
+    scrollToTopIcon.click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+
+    });
+
+
+
+
+
+
     $("label").click(function () {
 
         var filterVal = $(this).attr('data-filter');
