23e634abbd334f757a250c752dd42d20bcca6bb7
TaricOv <tareq.ouf@gmail.com>
2022-09-06 04:16:34 +0200

    Added some functionality to the Search bar: keyboard shortcut and conditions for showing on and going off

--- js/script.js
+++ js/script.js
@@ -42,4 +42,24 @@ $(document).ready(() => {
         });
     });
 
+
+    $(document).on("keydown", function (e) {
+        if (e.code === 'F3' || ((e.ctrlKey || e.metaKey) && e.code === 'KeyJ')) {
+            if (($(".search__container").css("top") == "152.797px")) $(".search__container").css({ top: "-20%" }); else
+                $(".search__container").css({ top: "20%" });
+            // $(".search__container_bar").focus(); 
+            $(".search__container_bar").val("");
+        }
+
+    })
+    $(document).click(function (e) {
+        if (($(".search__container").css("top") == "152.797px") && !e.target.closest(".search__container")) $(".search__container").css({ top: "-20%" });
+        $(".search__container_bar").val("");
+    })
+    $(".search__container").on('keyup', function (e) {
+        if (e.key === 'Enter' || e.keyCode === 13) {
+            console.log("searched!")
+        }
+    });
+
 })
