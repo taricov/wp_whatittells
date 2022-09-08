5b931c9526c3ef246f972ab35b7c8cd0cc202c05
TaricOv <tareq.ouf@gmail.com>
2022-09-08 09:08:53 +0200

    Search bar: it can now reads the input .. to be continued

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 $(document).ready(() => {
 
-
-
-
     var scrollToTopIcon = $(".scrollToTopIcon");
-    console.log(scrollToTopIcon)
+
     $(window).scroll(function () {
         var topPos = $(this).scrollTop();
         if (topPos > 100) {
@@ -43,6 +40,41 @@ $(document).ready(() => {
     });
 
 
+
+    // ==================== Search Bar =======================
+
+
+    function debounce(fn, threshold) {
+        var timeout;
+        return function debounced() {
+            if (timeout) {
+                clearTimeout(timeout);
+            }
+            function delayed() {
+                fn();
+                timeout = null;
+            }
+            timeout = setTimeout(delayed, threshold || 100);
+        }
+    }
+
+    let query, filterVal;
+
+    query = new RegExp($('.search__container_bar').val(), 'gi');
+    var searchBar = $('.search__container_bar').keyup(debounce(function () {
+        $('.projects__parent_container').isotope({
+            filter: function () {
+                $this = $(this)
+                let searchResult = query ? $this.text().match(query) : true;
+                console.log($this.text(), searchResult)
+                let projResult = filterVal ? $this.is(filterVal) : true;
+                return searchResult && projResult;
+            }
+        });
+    }))
+
+
+
     $(document).on("keydown", function (e) {
         if (e.code === 'F3' || ((e.ctrlKey || e.metaKey) && e.code === 'KeyJ')) {
             if (($(".search__container").css("top") == "152.797px")) $(".search__container").css({ top: "-20%" }); else
@@ -56,9 +88,15 @@ $(document).ready(() => {
         if (($(".search__container").css("top") == "152.797px") && !e.target.closest(".search__container")) $(".search__container").css({ top: "-20%" });
         $(".search__container_bar").val("");
     })
-    $(".search__container").on('keyup', function (e) {
-        if (e.key === 'Enter' || e.keyCode === 13) {
-            console.log("searched!")
+    // $(".search__container").on('keyup', function (e) {
+    //     if (e.key === "Enter" || e.keyCode === 13) {
+    //         console.log("searched!")
+    //     }
+    // });
+    $(document).on('keyup', function (e) {
+        if (($(".search__container").css("top") == "152.797px") && e.key === "Escape") {
+            $(".search__container").css({ top: "-20%" });
+            $(".search__container_bar").val("");
         }
     });
 
