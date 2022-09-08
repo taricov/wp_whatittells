145bd22a665b0bce5ea5c9fb5ff33ba781a6e54a
TaricOv <tareq.ouf@gmail.com>
2022-09-08 08:21:20 +0200

    Support team page: added the form in the latest update in the timeline ui ::FINISHD::

--- js/supportScript.js
+++ js/supportScript.js
@@ -1,18 +1,32 @@
 $(document).ready(() => {
 
     $(".timeline__container_list_item").map((idx, eachOne) => {
-        console.log(eachOne)
+        // console.log(eachOne)
         if ($(eachOne).data("status") === "needed") $(eachOne).addClass("needed_status")
 
     })
 
 
-    $(".timeline__container_list_item").click(function () {
+    $(".timeline__container_list_item").click(function (e) {
         // $this = $(this)
         $this = $(this).find($(".timeline__container_list_item_details"))
 
-        if ($this.css("display") != "none") $this.slideUp(500); else $this.slideDown(500)
+        if (e.target instanceof HTMLInputElement || e.target instanceof HTMLTextAreaElement || e.target instanceof HTMLButtonElement) {
+            console.log("no")
+        } else {
+            // if($this.hasClass("present"))
+
+            if ($this.css("display") != "none") {
+                $this.removeClass("present__details")
+                $this.slideUp(500)
+
+            } else {
+                $this.slideDown(500)
+
+            }
+        }
 
     })
+
 
 })
